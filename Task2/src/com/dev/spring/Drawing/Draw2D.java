65a6a345f294b057ac21546a35ebbb354379660e
ahmed radwan <tech.ahmedradwan@gmail.com>
2026-07-18 07:49:12 +0300

    solve task2

---
+++ Task2/src/com/dev/spring/Drawing/Draw2D.java
@@ -0,0 +1,7 @@
+package com.dev.spring.Drawing;
+
+public class Draw2D {
+    public void draw2d(String shape){
+        System.out.printf("draw 2d of shape %s\n",shape);
+    }
+}
---
+++ Task2/src/com/dev/spring/Drawing/Draw3D.java
@@ -0,0 +1,7 @@
+package com.dev.spring.Drawing;
+
+public class Draw3D {
+public void draw3d(String shape){
+    System.out.printf("drawing 3d for shape %s\n",shape);
+}
+}
